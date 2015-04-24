b6268dd0cf1ea04a5f2f5244938db2c80a26d358
Anna Shipman <anna@annashipman.co.uk>
2015-04-24 16:49:36 +0100

    Not sure how to make it die
    Leaving this for now, just wondering.

--- tests/test.pl
+++ tests/test.pl
@@ -7,4 +7,7 @@ Log::new('tests/test.csv');
 my $result_key   = "Anna";
 is( $result_key, "Anna", "Test does nothing yet" );
 
+Log::new('tests/not_csv.csv');
+is (1, 1, "what do I need to do to make it die?");
+
 done_testing();
