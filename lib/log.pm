59523c32af7e52a872d01edcc3cac789614f26bc
Anna Shipman <anna@annashipman.co.uk>
2015-04-30 11:05:48 +0100

    Take input from the command line
    This is in preparation for cleaning the data after parsing it. I haven't yet worked out how to test this.

--- lib/log.pm
+++ lib/log.pm
@@ -12,9 +12,14 @@ sub new {
     my $csv = Text::CSV->new ( { binary => 1 } )
         or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
-    open my $fh, "<:encoding(utf8)", $csv_name or die $csv_name . ": $!";
+    open my $fh, "<:encoding(utf8)", $csv_name
+        or die "Cannot use " . $csv_name . ": $!";
+
         while ( my $row = $csv->getline( $fh ) ) {
-            print $row->[0] . "\n";
+            my $time = $row->[0];
+            print "is time correct?";
+            chomp(my $answer = <>);
+            print $answer;
             push @rows, $row;
         }
     $csv->eof or $csv->error_diag();
