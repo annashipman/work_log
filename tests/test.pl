cbc6363cc67560b6dd52fa40cba3652e5958fcbf
Anna Shipman <anna@annashipman.co.uk>
2015-10-30 22:13:12 +0100

    Expected minute outputs

--- tests/test.pl
+++ tests/test.pl
@@ -9,6 +9,15 @@ Log::new('tests/test.csv');
 my $csv = Text::CSV->new ( { binary => 1 } ) or die "died";
 open my $fh, "<", "out/new.csv", or die "could not open out/new.csv: $!";
 
+# At this stage, I am expecting the output to be
+# some kind of error or something for each line with a date
+# and each blank line
+# and a correct number of minutes for each time
+# and the correct minutes are:
+
+# 10 55 25 15 15 10 5 5 10 20 5 10 15 20 20 35 5 15 10 60 5 15
+# 50 55 10 5 10 10 20 35 50 15 20 10 10 15 15 20
+
 my $row = $csv->getline( $fh );
 is_deeply ( $row, ["A0","A1","A2","A3"], "A row is yes" );
 
