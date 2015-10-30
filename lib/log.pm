3da600ff4d19c56c312465a0b7e32b6f770bed62
Anna Shipman <anna@annashipman.co.uk>
2015-10-30 18:40:11 +0100

    First steps towards pulling out the time
    I am hampered in my desire to search for better solutions by lack of internet,
    as detailed in the comments (for now...). This is a first stab at a very
    re-inventing the wheel way of pulling out the times. For now it just fakes the
    time and puts it into th new spreadsheet.
    
    Lack of internet also prevents me testing this as I don't have all the
    dependencies locally and my Vagrant VM is not running. Tests will certainly fail
    at this point and I have not begun updating them yet.

--- lib/log.pm
+++ lib/log.pm
@@ -17,7 +17,32 @@ sub new {
 
     while ( my $row = $csv->getline( $fh ) ) {
         my $time = $row->[0];
-        print $time;
+
+        # work out how many minutes in the time I've been given
+        # no handling of errors, blank lines, day headimgs yet
+
+§       # So, assume all times are correctly formatted
+        # 24hr, split with a hyphen
+        # Hmm... would like to split at hyphen but without the internet
+        # cannot guess how. So instead, assume they are all of the format
+        # HH.MM-HH.MM in the 24-hour clock
+
+        my $start_hr  = substr $time, 0, 1;
+        my $start_min = substr $time, 3, 4;
+        my $end_hr    = substr $time, 6, 7;
+        my $end_min   = substr $time, 9, 10;
+
+        # wow, that's hacky
+        # now what? I feel like I'm reinventing the wheel here
+        # surely there are time functions in perl
+        # but I can't guess what they're called to search the docs
+
+        # fake the calculation for now
+        my $time_elapsed = 70;
+
+
+        $row->[0] = $time_elapsed;
+
         push @rows, $row;
     }
     $csv->eof or $csv->error_diag();
