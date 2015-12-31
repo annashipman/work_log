905882c97172ffd68ac794c16a31d8d0082b41f0
Anna Shipman <anna@annashipman.co.uk>
2015-12-31 14:29:00 +0000

    Add very naive test
    This just tests we are where I think we are, by testing the first few lines of
    the output. The first line and the 0s should not appear, but at least we know
    where we are now.

--- tests/test.pl
+++ tests/test.pl
@@ -19,13 +19,25 @@ open my $fh, "<", "out/new.csv", or die "could not open out/new.csv: $!";
 # 50 55 10 5 10 10 20 35 50 15 20 10 10 15 15 20
 
 my $row = $csv->getline( $fh );
-is_deeply ( $row, ["A0","A1","A2","A3"], "A row is yes" );
+my $time = $row->[0];
+is ( $time, 2 );
 
 $row = $csv->getline( $fh );
-is_deeply ( $row, ["C0","C1","C2","C3"], "B row was 'no' so 2nd row is C" );
+$time = $row->[0];
+is ( $time, 0 );
 
 $row = $csv->getline( $fh );
-is ($row, undef, "There are only two rows");
+$time = $row->[0];
+is ( $time, 70 );
+
+$row = $csv->getline( $fh );
+$time = $row->[0];
+is ( $time, 0 );
+
+$row = $csv->getline( $fh );
+$time = $row->[0];
+is ( $time, 55 );
+
 
 
 done_testing();
