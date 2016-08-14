c0b1ff1795a1eabac3f46fb3385f280b25e4e294
Anna Shipman <anna@annashipman.co.uk>
2016-08-14 10:01:47 +0100

    Move all tests into the context
    This commit doesn't change much but will make it easier to see changes in subequent commits.
    
    "Reading the input" is also not a great name for the context but that illustrates the problem I'm attempting to address; it's hard to remember what I was doing/planning. When I'm back in the mindset of this project hopefully I will come up with a better name.

--- tests/test.pl
+++ tests/test.pl
@@ -9,144 +9,143 @@ Log::new('tests/test.csv', $output_csv);
 my $csv = Text::CSV->new ( { binary => 1 } ) or die "died";
 open my $fh, "<", $output_csv, or die "could not open $output_csv: $!";
 
-
-my $row = $csv->getline( $fh );
-is_deeply ( $row, ["Workday"] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["70","Sprint planning",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["55","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["25","Blog post",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["15","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["15","Reading up about shared parental leave",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["5","Blog post",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["5","'Helping with comms' Betony asked me not Carl",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Talking to Carl: general strategy stuff",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["20","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["5","Blog post - Frances coming back to me ages later after having already talked to Betony",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["15","Job spec",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["20","My own appraisal stuff",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["20","UBS talk",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["35","Follow up with Liam on reverse mentoring",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["5","Talk to Ahana",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["15","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Lists",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["60","Reading",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["5","Pivotal story",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["15","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["Workday"] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["50","Blog post",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["55","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Job spec",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["5","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Looking into ticketing systems",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["20","Enabling strategy show and tell",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["35","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["50","Retrospective",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["75","John Manzoni Q & A",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["20","Chat with Dan about tech & job spec",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["10","Talking to Torben about rates",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["15","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["15","Blog post",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["20","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["Holiday"] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["55","Email",""] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["Holiday"] );
-
-$row = $csv->getline( $fh );
-is_deeply ( $row, ["50","Email",""] );
-
 describe "reading the input" => sub {
+
+  my $row = $csv->getline( $fh );
+  is_deeply ( $row, ["Workday"] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["70","Sprint planning",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["55","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["25","Blog post",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["15","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["15","Reading up about shared parental leave",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["5","Blog post",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["5","'Helping with comms' Betony asked me not Carl",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Talking to Carl: general strategy stuff",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["20","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["5","Blog post - Frances coming back to me ages later after having already talked to Betony",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["15","Job spec",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["20","My own appraisal stuff",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["20","UBS talk",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["35","Follow up with Liam on reverse mentoring",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["5","Talk to Ahana",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["15","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Lists",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["60","Reading",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["5","Pivotal story",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["15","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["Workday"] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["50","Blog post",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["55","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Job spec",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["5","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Looking into ticketing systems",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["20","Enabling strategy show and tell",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["35","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["50","Retrospective",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["75","John Manzoni Q & A",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["20","Chat with Dan about tech & job spec",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["10","Talking to Torben about rates",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["15","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["15","Blog post",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["20","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["Holiday"] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["55","Email",""] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["Holiday"] );
+
+  $row = $csv->getline( $fh );
+  is_deeply ( $row, ["50","Email",""] );
+
   it "identifies holidays" => sub {
     $row = $csv->getline( $fh );
     is_deeply ( $row, ["Holiday"] );
-
     $row = $csv->getline( $fh );
     is_deeply ( $row, ["45","Email",""] );
   };
