1f2aa3cf089ed711595f7934f9d87de00140b23a
Anna Shipman <anna@annashipman.co.uk>
2016-09-06 23:13:55 +0100

    Use Getopts to set input and output csvs

--- wl.pl
+++ wl.pl
@@ -3,9 +3,23 @@
 use strict;
 use warnings;
 use Log;
+use Getopt::Std;
 
-my $input_csv = shift || 'tests/test.csv';
-my $output_csv = shift || 'new.csv';
+
+my %options=();
+getopts("hi:o:", \%options);
+
+my $input_csv   = ( $options{i} || 'tests/test.csv' );
+my $output_csv  = ( $options{o} || 'new.csv' );
+
+if ( $options{h} )
+{
+  print_help();
+}
 
 Log::new($input_csv, $output_csv);
 
+sub print_help {
+  print "-i   Input CSV\n";
+  print "-o   Output CSV\n";
+}
